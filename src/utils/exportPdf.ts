8a0fda38b16fd2e52009339e3b62eda97075364c
ufik0203@gmail.com <ufik0203@gmail.com>
2026-06-04 11:41:04 +0700

    update: size

--- src/utils/exportPdf.ts
+++ src/utils/exportPdf.ts
@@ -5,11 +5,25 @@ export const exportPredictionPdf = (
   uploadedData: any[],
   predictions: any[],
 ) => {
-  const doc = new jsPDF("landscape");
+  const doc = new jsPDF({
+    orientation: "landscape",
+    unit: "mm",
+    format: "a4",
+  });
 
+  doc.setFont("times", "bold");
   doc.setFontSize(18);
 
-  doc.text("Laporan Hasil Klasifikasi Anemia", 14, 15);
+  // doc.text("Laporan Hasil Klasifikasi Anemia", 14, 15);
+
+  doc.text(
+    "Laporan Hasil Klasifikasi Anemia",
+    doc.internal.pageSize.getWidth() / 2,
+    15,
+    {
+      align: "center",
+    },
+  );
 
   const diagnosisCount = predictions.reduce(
     (acc: Record<string, number>, curr: any) => {
@@ -42,8 +56,20 @@ export const exportPredictionPdf = (
 
   y += 8;
 
+  // Object.entries(diagnosisCount).forEach(([key, value]) => {
+  //   doc.text(`${key}: ${value}`, 20, y);
+
+  //   y += 6;
+  // });
+
   Object.entries(diagnosisCount).forEach(([key, value]) => {
-    doc.text(`${key}: ${value}`, 20, y);
+    doc.setTextColor(255, 0, 0);
+    doc.text(`${key}: `, 20, y);
+
+    const textWidth = doc.getTextWidth(`${key}: `);
+
+    doc.setTextColor(0, 0, 0);
+    doc.text(String(value), 20 + textWidth, y);
 
     y += 6;
   });
